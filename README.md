45518fbd9c5b298ac9fafc91253c29286d8d696a
Skryptex-Devs <devs@skryptex.com>
2015-02-03 20:03:21 +0000

    1

---
+++ README.md
@@ -0,0 +1,10 @@
+========================
+--------Skryptex-------- 
+========================
+http://www.skryptex.org
+Copyright (c) 2009-2010 Satoshi Nakamoto
+Copyright (c) 2009-2014 The Bitcoin developers
+Copyright (c) 2011-2014 The Litecoin Developers
+Copyright (c) 2014-Present The Skryptex Devs
+Distributed under the MIT/X11 software license, see the accompanying
+file COPYING or http://www.opensource.org/licenses/mit-license.php.
---
+++ src/qt/aboutdialog.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2011-2014 The Litecoin Developers
+// Copyright (c) 2014-Present The Skryptex Devs
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ABOUTDIALOG_H
+#define ABOUTDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class AboutDialog;
+}
+class ClientModel;
+
+/** "About" dialog box */
+class AboutDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AboutDialog(QWidget *parent = 0);
+    ~AboutDialog();
+
+    void setModel(ClientModel *model);
+
+private:
+    Ui::AboutDialog *ui;
+
+private slots:
+    void on_buttonBox_accepted();
+};
+
+#endif // ABOUTDIALOG_H
